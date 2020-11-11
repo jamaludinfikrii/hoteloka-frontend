fe33c398b38a94a7758b9a1afd0e4409e6881422
fikri <me@fikri.tech>
2020-11-11 14:11:34 +0700

    react-native image picker

--- src/screens/MyAccount/MyAccount.js
+++ src/screens/MyAccount/MyAccount.js
@@ -1,15 +1,36 @@
-import { Button, Container, Content, Text } from 'native-base'
+import { Button, Container, Content, H1, Text } from 'native-base'
 import React from 'react'
 import {onLogotCLick} from './../../redux/actions/userActions'
 import {connect} from 'react-redux'
+import ImagePicker from 'react-native-image-picker';
 
 const MyAccount = ({onLogotCLick}) => {
+
+    const onChangeImage = () => {
+        ImagePicker.showImagePicker({ storageOptions: { privateDirectory: true } },(response) => {
+            console.log('Response = ', response);
+ 
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+            } else {
+              const source = { uri: response.uri };
+            }
+        })
+    }
+
     return(
         <Container>
             <Content>
-                <Text>Ini MyAccount</Text>
+                <H1>My Account</H1>
                 <Button rounded danger onPress={onLogotCLick}>
                     <Text>Logout</Text>
+                </Button>
+                <Button rounded danger onPress={onChangeImage}>
+                    <Text>Change Image Profile</Text>
                 </Button>
             </Content>
         </Container>
